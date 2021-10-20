4a69256e6b366f38e4af1e31f0066770f5b4b800
Laxit Shahi <shahilax@my.yorku.ca>
2021-10-20 17:24:47 -0400

    L10 complete

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
+            cart: [],
             premium: true
         }
     },
-    methods: {}
+    methods: {
+        updateCart(id) {
+            this.cart.push(id)
+        },
+        removeItem(id) {
+            const index = this.cart.indexOf(id)
+            if(index > -1){
+                this.cart.splice(index,1)
+            }
+            // output = this.cart.filter(item => item !== id )
+            // this.cart = output
+        }
+    }
 })
